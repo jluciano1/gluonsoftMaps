f3609f7409bd9bed06ebb9be512c7dc684c4a4c0
jluciano1 <jose.oliveira@techne.com.br>
2016-04-19 18:26:54 -0300

    ajustes

--- webcontent/js/events.js
+++ webcontent/js/events.js
@@ -1,23 +1,19 @@
 var geocoder;
 var map;
-var marker;
 var autocomplete;
+var initPosition;
+var first;
 
 function initialize() {
-  var latlng = new google.maps.LatLng(-23.5652103, -46.65112599999998);
+  initPosition = new google.maps.LatLng(-23.5652103, -46.65112599999998);
   var options = {
 		zoom: 5,
-		center: latlng,
+		center: initPosition,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var div = $('.map');
 	map = new google.maps.Map(div[0], options);
 	geocoder = new google.maps.Geocoder();
-	marker = new google.maps.Marker({
-		map: map,
-		draggable: true,
-	});
-	marker.setPosition(latlng);
 	var autoCompleteInput = $('.map-control');
 	autoCompleteInput = autoCompleteInput[0];
   autoCompleteOpcoes = {
@@ -28,26 +24,32 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
     var data = $('.map-control').serialize();
     app.userEvents.fetchMap();
   });
+  map.setCenter(initPosition);
 }
  
 app.userEvents.fetchMap = function()
 {
-  if($($('.map-control').val()).val() != "")
+  if (!first)
   {
-    this.carregarNoMapa($('.map-control').val());
+    first = true
+    this.loadMap("Aclimação, São Paulo - SP, Brasil");
+  }
+  else if($($('.map-control').val()).val() !== "")
+  {
+    this.loadMap($('.map-control').val());
   }	
   return true;
 }
 
-app.userEvents.carregarNoMapa = function(endereco)  {
+app.userEvents.loadMap = function(endereco)  {
   initialize();
 	geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
+		  console.log("passou aqui");
 			if (results[0]) {
 				var latitude = results[0].geometry.location.lat();
 				var longitude = results[0].geometry.location.lng();
 				var location = new google.maps.LatLng(latitude, longitude);
-				marker.setPosition(location);
 				map.setCenter(location);
 				map.setZoom(16);
 			}
