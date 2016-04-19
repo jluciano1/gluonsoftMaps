083f4714ad486b25ea5806116c3539dc64f27a94
jluciano1 <jose.oliveira@techne.com.br>
2016-04-19 17:52:31 -0300

    finalizando

--- webcontent/js/events.js
+++ webcontent/js/events.js
@@ -1,110 +1,56 @@
 var geocoder;
 var map;
 var marker;
+var autocomplete;
 
 function initialize() {
-	var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
-	var options = {
+  var latlng = new google.maps.LatLng(-23.5652103, -46.65112599999998);
+  var options = {
 		zoom: 5,
 		center: latlng,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	
-	map = new google.maps.Map(document.getElementById("mapa"), options);
-	
+	var div = $('.map');
+	map = new google.maps.Map(div[0], options);
 	geocoder = new google.maps.Geocoder();
-	
 	marker = new google.maps.Marker({
 		map: map,
 		draggable: true,
 	});
-	
 	marker.setPosition(latlng);
+	var autoCompleteInput = $('.map-control');
+	autoCompleteInput = autoCompleteInput[0];
+  autoCompleteOpcoes = {
+  types: ['geocode']
 }
-
+autocomplete = new google.maps.places.Autocomplete(autoCompleteInput,autoCompleteOpcoes);
+google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var data = $('.map-control').serialize();
+    app.userEvents.fetchMap();
+  });
+}
+ 
 app.userEvents.fetchMap = function()
 {
-  if($(this.address).val() != "")
+  if($($('.map-control').val()).val() != "")
   {
-    this.carregarNoMapa(this.address);
+    this.carregarNoMapa($('.map-control').val());
   }	
-			
-		return true;
+  return true;
 }
 
-
- app.userEvents.carregarNoMapa = function(endereco)  {
-
-	initialize();
-	
-	
-		geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				if (results[0]) {
-					var latitude = results[0].geometry.location.lat();
-					var longitude = results[0].geometry.location.lng();
-		
-					//$('#txtEndereco').val(results[0].formatted_address);
-					//$('#txtLatitude').val(latitude);
-          //         	$('#txtLongitude').val(longitude);
-		
-					var location = new google.maps.LatLng(latitude, longitude);
-					console.log(location);
-					marker.setPosition(location);
-					map.setCenter(location);
-					map.setZoom(16);
-				}
+app.userEvents.carregarNoMapa = function(endereco)  {
+  initialize();
+	geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			if (results[0]) {
+				var latitude = results[0].geometry.location.lat();
+				var longitude = results[0].geometry.location.lng();
+				var location = new google.maps.LatLng(latitude, longitude);
+				marker.setPosition(location);
+				map.setCenter(location);
+				map.setZoom(16);
 			}
-		})
-	}
-
-
-	
-	google.maps.event.addListener(marker, 'drag', function () {
-		geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				if (results[0]) {  
-					//$('#txtEndereco').val(results[0].formatted_address);
-					//$('#txtLatitude').val(marker.getPosition().lat());
-					//$('#txtLongitude').val(marker.getPosition().lng());
-				}
-			}
-		});
-	});
-	
-	app.userEvents.autocomplete = function() {
-			console.log("to digitando");
-			geocoder.geocode({ 'address': request.term + ', Brasil', 'region': 'BR' }, function (results, status) {
-				response($.map(results, function (item) {
-					return {
-						label: item.formatted_address,
-						value: item.formatted_address,
-						latitude: item.geometry.location.lat(),
-          				longitude: item.geometry.location.lng()
-					}
-				}));
-			})
 		}
-		
-	app.userEvents.autocompleteselect = function () {
-			//$("#txtLatitude").val(ui.item.latitude);
-//    		$("#txtLongitude").val(ui.item.longitude);
-			console.log("to clicando");
-			var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-			marker.setPosition(location);
-			map.setCenter(location);
-			map.setZoom(16);
-			
-		}
-	
-	
-	$("form").submit(function(event) {
-		event.preventDefault();
-		
-		//var endereco = $("#txtEndereco").val();
-		//var latitude = $("#txtLatitude").val();
-		//var longitude = $("#txtLongitude").val();
-		
-		//alert("Endereço: " + endereco + "\nLatitude: " + latitude + "\nLongitude: " + longitude);
-	
-});
+	})
+}
